71d81e6dad75ae524b0955ef3a4dd1534c6b3af8
borishdez <bhernandez@bitworks.com.sv>
2024-09-18 19:31:31 -0600

    favoritos segun clasificacion mg y rep

--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
 import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
@@ -16,11 +17,22 @@ public class Principal {
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 2000; i++) {
             miCancion.reproduce();
+        }
+
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
         }
 
         System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
         System.out.println("Total de Me Gusta: " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adiciones(miPodcast);
+        favoritos.adiciones(miCancion);
     }
 }
